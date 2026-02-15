eda1c2f7536a03039dfaa0663e0c0b014f732c30
pranavsirsufale <pranavdada334@gmail.com>
2026-02-15 18:39:56 +0530

    udpate requirements file with latest packages installed

--- smartStore/smartStore/settings.py
+++ smartStore/smartStore/settings.py
@@ -44,10 +44,7 @@ DATABASES = {
         "USER": "store",
         "PASSWORD": "Store@123",
         "HOST": "localhost",
-        "PORT": "3306",
-        # "OPTIONS": {
-        #     "init_command": "SET sql_mode='STRICT_TRANS_TABLES"
-        # }
+        "PORT": "3306"
     }
 }
 AUTH_PASSWORD_VALIDATORS = [
